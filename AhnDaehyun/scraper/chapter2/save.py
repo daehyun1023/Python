596cb5be0fbea3e4bd66fcacbccec88c6ae2c4f3
daehyun1023 <daehyun1023@naver.com>
2021-02-04 12:31:07 +0900

    [0203]Flask apply

--- AhnDaehyun/scraper/chapter2/save.py
+++ AhnDaehyun/scraper/chapter2/save.py
@@ -1,10 +1,9 @@
 import csv
 
-def save_to_file():
+def save_to_file(jobs):
     file = open('jobs.csv', 'w', -1,'utf-8', newline='')
     writer = csv.writer(file)
     writer.writerow(['title', 'company', 'location', 'apply_link'])
-    writer.writerow([1, 2, 3, 4])
     for job in jobs:
         writer.writerow(list(job.values()))
     return 
