99d3c1badd31c3aef2bc9f4fe52296768c5c117e
daehyun1023 <daehyun1023@naver.com>
2021-02-08 08:54:31 +0900

    exporter.py modification

--- AhnDaehyun/scraper/clone-project/exporter.py
+++ AhnDaehyun/scraper/clone-project/exporter.py
@@ -2,7 +2,7 @@ import csv
 
 
 def save_to_file(jobs):
-    file = open(f"{jobs}.csv", "w", -1, "utf-8", newline="")
+    file = open("jobs.csv", "w", -1, "utf-8", newline="")
     writer = csv.writer(file)
     writer.writerow(['Title', 'Company', 'Apply_link'])
     for job in jobs:
